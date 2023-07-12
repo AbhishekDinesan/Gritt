9ed17cc8f54fa5c063a5965fe274c2be6cdc0be1
Abhishek Dinesan <abhidines04@gmail.com>
2023-07-11 23:36:56 -0400

    smallchange

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import {View, Text, SafeAreaView, StyleSheet } from 'react-native';
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import SignInScreen from './app/screens/SignInScreen';
+import Navigation from './app/navigation';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import 'react-native-gesture-handler';
+
 
 export default function App() {
- // return <WelcomeScreen />;
- return <SignInScreen />;
+ //return <WelcomeScreen />;
+ //return <SignInScreen />;
+ //return <SignUpScreen />
+ //return <ConfirmEmailScreen />
+ //return <ForgotPasswordScreen />
+ return <Navigation />
 }
 
 const styles = StyleSheet.create({
